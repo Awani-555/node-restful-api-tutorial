c9fe2b251e7c22e0cc80b2f98e574f22fd2b859e
Awani-555 <awaniarsh05@gmail.com>
2025-07-30 21:28:44 +0530

    Succesfully made GET,POST,PUT and DELETE Functions

--- app.js
+++ app.js
@@ -6,13 +6,13 @@ app.use(express.json());
         { id:1,title:"Book 1",price: 250.50},
         { id:2,title:"Book 2",price: 450.50}
     ]
-
-
+//GET
 app.get('/products', (req,res) => {
    
     res.status(200).json({products});
 });
 
+//POST
 app.post('/products',(req,res) => {
  // 1. Get the data from the request body
   const { title, price } = req.body;
@@ -36,10 +36,52 @@ app.post('/products',(req,res) => {
  
   // 6. Respond with 201 Created status and the new product
   res.status(201).json(newProduct);
+})
 
 
+//PUT
+app.put('/products/:id', (req,res) => {
+    const id =parseInt(req.params.id); // Get the ID from URL and convert to number
+    const { title,price } = req.body;// Get new data from body
+
+    //Find index of product to update
+    const index = products.findIndex(p => p.id === id);
+
+     if (index === -1) {
+    // No product found with that ID
+    return res.status(404).json({ message: 'Product not found' });
+  }
+
+   // Update the product fields, only if provided
+  if (title !== undefined) products[index].title = title;
+  if (price !== undefined) products[index].price = price;
+
+  // Respond with updated product
+  res.status(200).json(products[index]);
+})
+
+//DELETE
+
+app.delete('/products/:id',(req,res) => {
+  const id= parseInt(req.params.id);
+  const index=products.find(p =>p.id === id);
+  if(index === -1){
+    return res.status(404).json({message: "Product not found "});
+  }
+
+  
+  // Remove product from array
+  products.splice(index, 1);
+
+  // 204 means "No Content" — success with no response body
+  res.status(204).send();
 
 })
+
+
+
+
+
 app.use((req, res) => {
     res.status(404).json({ message: "Route not found" });
 });
